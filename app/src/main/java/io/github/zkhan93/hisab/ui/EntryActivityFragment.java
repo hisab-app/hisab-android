9d9a77f8848be6c2195e895f444b34a919621b85
Zeeshan Khan <zkhan1093@gmail.com>
2016-06-26 02:31:08 +0530

    added group item layout, added expense item layout, added text view styles using card view

--- app/src/main/java/io/github/zkhan93/hisab/ui/EntryActivityFragment.java
+++ app/src/main/java/io/github/zkhan93/hisab/ui/EntryActivityFragment.java
@@ -1,17 +1,33 @@
 package io.github.zkhan93.hisab.ui;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.zkhan93.hisab.R;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class EntryActivityFragment extends Fragment {
+public class EntryActivityFragment extends Fragment implements View.OnClickListener {
+    public static final String TAG = EntryActivityFragment.class.getSimpleName();
+
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.btn_register)
+    Button btnRegister;
+    @BindView(R.id.btn_quit)
+    Button btnQuit;
+    @BindView(R.id.btn_confusion)
+    Button btnConfusion;
 
     public EntryActivityFragment() {
     }
@@ -19,6 +35,54 @@ public class EntryActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_entry, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_entry, container, false);
+        ButterKnife.bind(this, rootView);
+
+        if (btnLogin != null)
+            btnLogin.setOnClickListener(this);
+        if (btnRegister != null)
+            btnRegister.setOnClickListener(this);
+        if (btnQuit != null)
+            btnQuit.setOnClickListener(this);
+        if (btnConfusion != null)
+            btnConfusion.setOnClickListener(this);
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                loginClick();
+                break;
+            case R.id.btn_register:
+                registerClick();
+                break;
+            case R.id.btn_confusion:
+                confusionClick();
+                break;
+            case R.id.btn_quit:
+                quitClick();
+                break;
+            default:
+                Log.d(TAG, "click not implemented");
+        }
+
+    }
+
+    public void loginClick() {
+        startActivity(new Intent(getActivity(), GroupsActivity.class));
+    }
+
+    public void registerClick() {
+        Toast.makeText(getActivity(), "Register", Toast.LENGTH_SHORT).show();
+    }
+
+    public void quitClick() {
+        Toast.makeText(getActivity(), "Quit", Toast.LENGTH_SHORT).show();
+    }
+
+    public void confusionClick() {
+        Toast.makeText(getActivity(), "Confuse", Toast.LENGTH_SHORT).show();
     }
 }
